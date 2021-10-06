6cc5a6c9f7b29bc05a858c42386e61a3a6fc11a2
Weijia Cheng <weijia_cheng1@baylor.edu>
2021-10-06 16:34:32 -0500

    10.6

--- List Comprehension Start File.py
+++ List Comprehension Start File.py
@@ -54,7 +54,6 @@ print(new_list)
 x = [i for i in range(10)]
 print(x)
 
-
 # Output -[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
@@ -69,7 +68,6 @@ print(x)
 list1 = [3, 4, 5]
 multiplied = [item*3 for item in list1]
 print(multiplied)
-
 
 # [9,12,15]
 
@@ -87,22 +85,25 @@ lower = [x.lower() for x in ["A", "B", "C"]]
 print(lower)
 
 # Output 1 - ['a', 'b', 'c']
+
 lower = [x.upper() for x in ["a", "b", "c"]]
 print(lower)
-
 
 # Output 2 - ['A', 'B', 'C']
 
 
 # 6) Creating a list based on a condition
-
+a = [i*i for i in range(5) if i % 2 == 0]
+print(a)
 
 #Output - [0, 4, 16]
 
 
 # 7) Extracting numbers only from a string and putting it in a list
-
-
+string = "Hello 12345 World"
+numbers = [x for x in string if x.isdigit()]
+# numbers = [x for x in string if x.isalpha()]
+print(numbers)
 # Output - ['1', '2', '3', '4', '5']
 
 
@@ -119,35 +120,38 @@ this is line4
 this is line5
 
 Save the file as test.txt '''
+fn = open("test.txt", 'r')
 
-
+result = [i for i in fn if "line3" in i]
+print(result)
 #Output: ['this is line3']
 
 
 # 9) Using functions in list comprehension
 
 # Create a function and name it double:
+def double(x):
+    return x*2
 
 
 # If you now just print that function with a value in it, it should look like this:
-
-
+print(double(10))
 # Answer - 20
 
 
 # We can easily use list comprehension on that function.
-
-
+answer = [double(x) for x in range(10)]
+print(answer)
 # Output - [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
 
 
 # 10) adding an IF condition to the above
-
-
+answer = [double(x) for x in range(10) if x % 2 == 0]
+print(answer)
 # Output - [0, 4, 8, 12, 16]
 
 
 # 11) You can add more arguments (using multiple iterators and lists):
-
-
+a = [x+y for x in [10, 30, 50] for y in [20, 40, 60]]
+print(a)
 # Output - [30, 50, 70, 50, 70, 90, 70, 90, 110]
--- json2.py
+++ json2.py
@@ -14,17 +14,18 @@ print(len(eqdata["features"]))
 
 list_of_eqs = eqdata["features"]
 
-mags = []
-lats = []
-lons = []
+mags, lats, lons, hover_texts = [], [], [], []
+
 
 for eq in list_of_eqs:
     mag = eq['properties']['mag']
     lat = eq['geometry']['coordinates'][1]
     lon = eq['geometry']['coordinates'][0]
+    title = eq['properties']['title']
     mags.append(mag)
     lats.append(lat)
     lons.append(lon)
+    hover_texts.append(title)
 
 
 print(mags[:5])
@@ -32,7 +33,22 @@ print(lats[:5])
 print(lons[:5])
 
 
-data = [Scattergeo(lon=lons, lat=lats)]
+#data = [Scattergeo(lon=lons, lat=lats)]
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*m for m in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title': 'Magnitude'}
+    }
+}]
+
 
 my_layout = Layout(title="Global Earthquakes 1 Day")
 
