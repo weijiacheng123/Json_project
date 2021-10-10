6919309c68e218f69427b82073ed925b0d8d0174
weijiacheng123 <weijia_cheng1@baylor.edu>
2021-10-10 17:01:31 -0500

    add JsonProjectAE folder

---
+++ JSON_Project_AE/9.1-9.13.py
@@ -0,0 +1,58 @@
+from plotly import offline
+from plotly.graph_objs import Scattergeo, Layout
+import plotly
+import json
+
+
+infile = open('US_fires_9_1.json', 'r')
+outfile = open('readable_eq_data.json', 'w')
+
+eqdata = json.load(infile)
+
+json.dump(eqdata, outfile, indent=4)
+
+print(len(eqdata["features"]))
+
+list_of_eqs = eqdata["features"]
+
+mags, lats, lons, hover_texts = [], [], [], []
+
+
+for eq in list_of_eqs:
+    mag = eq['properties']['mag']
+    lat = eq['geometry']['coordinates'][1]
+    lon = eq['geometry']['coordinates'][0]
+    title = eq['properties']['title']
+    mags.append(mag)
+    lats.append(lat)
+    lons.append(lon)
+    hover_texts.append(title)
+
+
+print(mags[:5])
+print(lats[:5])
+print(lons[:5])
+
+
+#data = [Scattergeo(lon=lons, lat=lats)]
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*m for m in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title': 'Magnitude'}
+    }
+}]
+
+
+my_layout = Layout(title="US Fires - 9/1/2020 through 9/13/2020")
+
+fig = {'data': data, 'layout': my_layout}
+
+offline.plot(fig, filename='USFires9/1-9/13.html')
